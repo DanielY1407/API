119fa1825e279cb7556adf78903ab703b852a6e7
DanielY1407 <101830114+DanielY1407@users.noreply.github.com>
2024-05-24 15:41:49 -0500

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.squareup.picasso:picasso:2.71828")
     implementation ("com.google.code.gson:gson:2.8.6")
     implementation ("com.android.volley:volley:1.2.0")
     implementation(libs.appcompat)
--- app/src/main/java/com/example/api/MainActivity.java
+++ app/src/main/java/com/example/api/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.api;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -8,19 +10,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.api.adaptadores.PersonajeAdaptador;
+import com.example.api.clases.Personaje;
 import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Personaje> ListaPersonaje = new ArrayList<>();
+
+    RecyclerView rcv_personaje;
+    Button btn_guardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +45,60 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        rcv_personaje = findViewById(R.id.rcv_personaje);
+        btn_guardar = findViewById(R.id.btn_guardar);
+
+        btn_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarInformacion();
+            }
+        });
 
         cargarInformacion();
+    }
+
+    private void guardarInformacion() {
+        HashMap<String, Object> json = new HashMap<>();
+        json.put("id", 101);
+        json.put("title", "foo");
+        json.put("body", "bar");
+        json.put("userId", 1);
+
+        JSONObject jsonObject = new JSONObject(json);
+
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        JsonObjectRequest myRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                respuestaGuardar(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Error en el servidor", Toast.LENGTH_LONG).show();
+            }
+
+        });
+        RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
+        rq.add(myRequest);
+
+    }
+
+    private void respuestaGuardar(JSONObject respuesta) {
+
+        try {
+            if (respuesta.getInt("id")==101){
+                Toast.makeText(getApplicationContext(), "Se guardó correctamenre", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "Error al guardar", Toast.LENGTH_LONG).show();
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Error en el servidor", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     private void cargarInformacion() {
@@ -58,8 +127,21 @@ public class MainActivity extends AppCompatActivity {
 
     private void recibirRespuesta(JSONObject respuesta) {
         try {
-            String nombre = respuesta.getJSONArray("results").getJSONObject(0).getString("name");
-            Toast.makeText(getApplicationContext(),nombre,Toast.LENGTH_LONG).show();
+            for (int i =0; i<=respuesta.getJSONArray("results").length();i++){
+
+                String nombre = respuesta.getJSONArray("results").getJSONObject(i).getString("name");
+                String estado = respuesta.getJSONArray("results").getJSONObject(i).getString("status");
+                String especie = respuesta.getJSONArray("results").getJSONObject(i).getString("species");
+                String imagen = respuesta.getJSONArray("results").getJSONObject(i).getString("image");
+
+                Personaje p = new Personaje(nombre,estado, especie, imagen);
+
+                ListaPersonaje.add(p);
+                rcv_personaje.setLayoutManager(new LinearLayoutManager(this));
+                rcv_personaje.setAdapter(new PersonajeAdaptador(ListaPersonaje));
+            }
+
+
         }catch (JSONException e){
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), "Error en el servidor", Toast.LENGTH_LONG).show();
